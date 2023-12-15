985b1f96bbd0ea854e1dd2e5e30f0090f30e1c09
Rukshesh Talwar <rukshesh.talwar12@gmail.com>
2023-12-15 13:06:10 +0530

    feat: linked list traversal: iteratively, recursively, returning a List<String>

--- src/com/linkedlists/LinkedListDSA.java
+++ src/com/linkedlists/LinkedListDSA.java
@@ -1,5 +1,8 @@
 package com.linkedlists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedListDSA {
     public static void main(String[] args) {
         Node<String> a = new Node<>("10");
@@ -10,5 +13,41 @@ public class LinkedListDSA {
         a.next = b;
         b.next = c;
 
+        printListIteratively(a);
+        printListRecursively(a);
+        linkedListValues(a);
+    }
+
+    //Traversing each element of a LinkedList
+    static void printListIteratively(Node head) {
+        Node current = head;
+        while(current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
+
+    //Traversing the LinkedList Recursively
+    static void printListRecursively(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        System.out.println(head.value);
+        printListRecursively(head.next);
+    }
+
+    static List<String> linkedListValues(Node<String> head) {
+        List<String> listNodes = new ArrayList<>();
+        if (head == null) {
+            return listNodes;
+        }
+
+        Node<String> current = head;
+        while (current != null) {
+            listNodes.add(current.value);
+            current = current.next;
+        }
+        return listNodes;
     }
 }
