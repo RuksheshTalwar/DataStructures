e0f82f61985814a69a736550ef0ea5392ab7a564
Rukshesh Talwar <rukshesh.talwar12@gmail.com>
2024-03-20 14:03:04 +0530

    feat: solving more data structures problems

---
+++ src/com/leetcode/easy/MergeSortedArrays_88.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+public class MergeSortedArrays_88 {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,0,0,0};
+        int[] arr2 = {2,5,6};
+//        mergeSortedArrays(arr1,arr2);
+    }
+
+    private static void mergeSortedArrays(int[] arr1, int m, int[] arr2, int n) {
+
+        int i = m - 1;
+        int j;
+        int p = n - 1;
+
+
+
+
+
+    }
+
+}
---
+++ src/com/leetcode/easy/SwapWithoutExtraSpace.java
@@ -0,0 +1,13 @@
+package com.leetcode.easy;
+
+public class SwapWithoutExtraSpace {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        x = x + y; // x -> 15
+        y = x - y; // y -> 10
+        x = x - y; // x -> 5
+        System.out.println("After swapping:"
+                + " x = " + x + ", y = " + y);
+    }
+}
